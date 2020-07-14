a3e6d5ca3b7403a0c962eb09d15b4d86d47b36cf
tyooma <flying.alien.godzilla@gmail.com>
2020-07-14 10:01:26 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.237993ceab8c171497bfb95469a06636.js"
+  "./precache-manifest.989c377efa6448e1ea1f68da8f4487fb.js"
 );
 
 self.addEventListener('message', (event) => {
